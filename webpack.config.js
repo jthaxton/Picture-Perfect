0a0ea8e25bebcdd404f27dea3e57a0bf928b5eb2
Joe Thaxton <joethaxton@Joes-MacBook-Pro.local>
2018-10-09 14:54:12 -0700

    Ignore node_modules

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: entry: './frontend/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts')
+    filename: 'bundle.js',
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/env','@babel/react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+}
