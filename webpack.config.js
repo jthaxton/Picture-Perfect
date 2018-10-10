0feeea084a494302d500fa7d2cd14861154f5cd5
Joe Thaxton <14jthaxton@gmail.com>
2018-10-09 18:04:52 -0700

    User auth: User and Session models, controllers

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,24 @@ var path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: entry: './frontend/index.jsx',
+  entry: './frontend/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts')
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js',
     devtoolModuleFilenameTemplate: '[resourcePath]',
     devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: [/\.jsx?$/],
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/env','@babel/react']
-        }
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
       }
     ]
   },
