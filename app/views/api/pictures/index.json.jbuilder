cdcf278eb80863cb30100ec6a52077329bcee90c
Joe Thaxton <14jthaxton@gmail.com>
2018-10-12 17:40:28 -0700

    AWS start

--- app/views/api/pictures/index.json.jbuilder
+++ app/views/api/pictures/index.json.jbuilder
@@ -1,5 +1,6 @@
-json.array! @picture, do |picture|
-  json.extract! icture, :id, :title
+json.array! @picture do |picture|
+  json.extract! picture, :id
   json.photoUrl url_for(picture.photo)
 end
- 
+
+json.photoUrls @pictures.photos.map { |file| url_for(file) }
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
-    resources :pictures, only: [:index]
+    resources :pictures, only: [:create, :index]
   end
 
   root to: 'static_pages#root'
