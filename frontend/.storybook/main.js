3abec97326dee65723303e0654306196e05edc6f
14jthaxton <14jthaxton@gmail.com>
2020-05-03 19:28:31 -0700

    storybook

---
+++ frontend/.storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ['../storybook_components/**/*.stories.js'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+};
---
+++ frontend/storybook_components/post.stories.js
@@ -0,0 +1,6 @@
+import { Post } from './post';
+import React from 'react';
+
+export default { title: 'Post' };
+
+export const post = () => <Post/>;
